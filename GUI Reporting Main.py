b464c27c3df05245f187b630305f14c40cdc1424
Clark <rcthurs@gmail.com>
2023-04-21 14:04:35 -0400

    Fixed the issue where the Tab Group wouldn't Stretch with the rest of the window.

--- GUI Reporting Main.py
+++ GUI Reporting Main.py
@@ -1,5 +1,4 @@
 import PySimpleGUI as sg
-import webbrowser
 import webview
 import time
 import pandas as pd
@@ -25,30 +24,28 @@ BPAD_LEFT = ((20,10), (0, 0))
 BPAD_LEFT_INSIDE = (0, (10, 0))
 BPAD_RIGHT = ((10,20), (10, 0))
 
-# Creating frames
-# top_banner = [[sg.Text('Precision Genetics', font='Any 20', background_color='#1B2838', enable_events=True, grab=False), sg.Push(background_color='#1B2838'),
-#                sg.Text('Reporting Tools', font='Any 20', background_color='#1B2838')],]
-
 # Creating tabs
-tab_al_typ = [[sg.T('Proceed to Thermofishers Allele Typer')],
+tab_al_typ = [[sg.T('Proceed to Thermofishers Allele Typer', expand_x=True, expand_y=True)],
               [sg.T('maybe we can upload genotyper file to fix sample names?')],
               [sg.Push(), sg.Button('ThermoFisher', size=(14, 2), button_color=('#000000', '#C2D4D8')), sg.Push()]]
 
-tab_diplo = [[sg.T('Upload Files For Calculations')],
-             [sg.Push(),sg.Button('Upload Files', size=(14, 2), button_color=('#000000', '#C2D4D8')),
+tab_diplo = [[sg.T('Upload Files For Calculations', expand_x=True, expand_y=True)],
+             [sg.Push(), sg.Button('Upload Files', size=(14, 2), button_color=('#000000', '#C2D4D8')),
               sg.Button('Export Results', size=(14, 2), button_color=('#000000', '#C2D4D8')), sg.Push()]]
 
-tab_genx = [[sg.T('Convert files for Genxys upload')],
+tab_genx = [[sg.T('Convert files for Genxys upload', expand_x=True, expand_y=True)],
             [sg.Push(), sg.Button('Convert', size=(14, 2), button_color=('#000000', '#C2D4D8')), sg.Push()]]
 
 # Creating layout with tabs
-layout =[[sg.TabGroup([
-            [sg.Tab('Allele Typer', tab_al_typ, expand_x=True, expand_y=True)],
-            [sg.Tab('Diplotype Calculator', tab_diplo, expand_x=True, expand_y=True)],
-            [sg.Tab('Genxys File Conversion', tab_genx, expand_x=True, expand_y=True)]
-         ], background_color=DARK_HEADER_COLOR, tab_location='topleft', expand_x=True, expand_y=True)]]
+layout = [[sg.Stretch()],
+          [sg.TabGroup([
+            [sg.Tab('Allele Typer', tab_al_typ)],
+            [sg.Tab('Diplotype Calculator', tab_diplo)],
+            [sg.Tab('Genxys File Conversion', tab_genx)]
+          ], background_color=DARK_HEADER_COLOR, tab_location='topleft', expand_x=True, expand_y=True)],
+          [sg.Stretch()]]
 
-window = sg.Window('Reporting Tools', layout, resizable=True)
+window = sg.Window('Reporting Tools', layout, resizable=True, finalize=True)
 
 while True:
         event, values = window.read()
